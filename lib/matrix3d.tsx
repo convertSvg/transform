a616c68a67ca7dcd2fb4a95e9a8d96f48e8f7486
gaolu11 <768065158@qq.com>
2019-08-15 11:19:42 +0800

    feat: update matrix.tsx  and matrix3d.tsx

--- lib/matrix3d.tsx
+++ lib/matrix3d.tsx
@@ -1,16 +1,15 @@
-// https://developer.mozilla.org/zh-CN/docs/Web/CSS/transform-function/matrix
+// https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/matrix3d
 //a  c  0  tx  
 //b  d  0  ty 
 //0  0  1  tz  
 //0  0  0  1
-
-import { cos, sin, tan, toRad } from './utils'
+import { cos, sin, tan, atan, toRad, sqrt } from './utils'
 
 const Matrix3d = [
     1, 0, 0, 0,
     0, 1, 0, 0,
     0, 0, 1, 0,
-    0, 0, 0, 0
+    0, 0, 0, 1
 ]
 
 /**
@@ -18,13 +17,20 @@ const Matrix3d = [
  * @param m1  [a, c, 0, tx, b, d, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1]
  * @param m2  [a, c, 0, tx, b, d, 0, ty, 0, 0, 1, tz, 0, 0, 0, 1]
  */
-const  combine3d = (m1, m2) => {
-    return [
-      m1[0] * m2[0] + m1[1] * m2[4] + m1[2] * m2[8] + m1[3] * m2[12],  m1[0] * m2[1] + m1[1] * m2[5] + m1[2] * m2[9] + m1[3] * m2[13],  m1[0] * m2[2] + m1[1] * m2[6] + m1[2] * m2[10] + m1[3] * m2[14],  m1[0] * m2[3] + m1[1] * m2[7] + m1[2] * m2[11] + m1[3] * m2[15],
-      m1[4] * m2[0] + m1[5] * m2[4] + m1[6] * m2[8] + m1[7] * m2[12],  m1[4] * m2[1] + m1[5] * m2[5] + m1[6] * m2[9] + m1[7] * m2[13],  m1[4] * m2[2] + m1[5] * m2[6] + m1[6] * m2[10] + m1[7] * m2[14],  m1[4] * m2[3] + m1[5] * m2[7] + m1[6] * m2[11] + m1[7] * m2[15],
-      m1[8] * m2[0] + m1[9] * m2[4] + m1[10] * m2[8] + m1[11] * m2[12],  m1[8] * m2[1] + m1[9] * m2[5] + m1[10] * m2[9] + m1[11] * m2[13],  m1[8] * m2[2] + m1[9] * m2[6] + m1[10] * m2[10] + m1[11] * m2[14],  m1[8] * m2[3] + m1[9] * m2[7] + m1[10] * m2[11] + m1[11] * m2[15],
-      m1[12] * m2[0] + m1[13] * m2[4] + m1[14] * m2[8] + m1[15] * m2[12],  m1[12] * m2[1] + m1[13] * m2[5] + m1[14] * m2[9] + m1[15] * m2[13],  m1[12] * m2[2] + m1[13] * m2[6] + m1[14] * m2[10] + m1[15] * m2[14],  m1[12] * m2[3] + m1[13] * m2[7] + m1[14] * m2[11] + m1[15] * m2[15]
-    ]
+function combine3d (...arg) {
+    const args = [].slice.call(arguments) || []
+    let m1 = args[0] || Matrix3d
+    args.map((m2 ,idx) => {
+        if(idx >=1){
+            m1 = [
+                m1[0] * m2[0] + m1[1] * m2[4] + m1[2] * m2[8] + m1[3] * m2[12],  m1[0] * m2[1] + m1[1] * m2[5] + m1[2] * m2[9] + m1[3] * m2[13],  m1[0] * m2[2] + m1[1] * m2[6] + m1[2] * m2[10] + m1[3] * m2[14],  m1[0] * m2[3] + m1[1] * m2[7] + m1[2] * m2[11] + m1[3] * m2[15],
+                m1[4] * m2[0] + m1[5] * m2[4] + m1[6] * m2[8] + m1[7] * m2[12],  m1[4] * m2[1] + m1[5] * m2[5] + m1[6] * m2[9] + m1[7] * m2[13],  m1[4] * m2[2] + m1[5] * m2[6] + m1[6] * m2[10] + m1[7] * m2[14],  m1[4] * m2[3] + m1[5] * m2[7] + m1[6] * m2[11] + m1[7] * m2[15],
+                m1[8] * m2[0] + m1[9] * m2[4] + m1[10] * m2[8] + m1[11] * m2[12],  m1[8] * m2[1] + m1[9] * m2[5] + m1[10] * m2[9] + m1[11] * m2[13],  m1[8] * m2[2] + m1[9] * m2[6] + m1[10] * m2[10] + m1[11] * m2[14],  m1[8] * m2[3] + m1[9] * m2[7] + m1[10] * m2[11] + m1[11] * m2[15],
+                m1[12] * m2[0] + m1[13] * m2[4] + m1[14] * m2[8] + m1[15] * m2[12],  m1[12] * m2[1] + m1[13] * m2[5] + m1[14] * m2[9] + m1[15] * m2[13],  m1[12] * m2[2] + m1[13] * m2[6] + m1[14] * m2[10] + m1[15] * m2[14],  m1[12] * m2[3] + m1[13] * m2[7] + m1[14] * m2[11] + m1[15] * m2[15]
+              ]
+        }
+    })
+    return m1
   }
 
 const translate = (x, y=x) => {
@@ -32,7 +38,7 @@ const translate = (x, y=x) => {
         1, 0, 0, x,
         0, 1, 0, y,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -41,7 +47,7 @@ const translate3d = (x = 0, y = 0, z = 0) => {
         1, 0, 0, x,
         0, 1, 0, y,
         0, 0, 1, z,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -51,7 +57,7 @@ const translateX = (x) => {
         1, 0, 0, x,
         0, 1, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -60,7 +66,7 @@ const translateY = (y) => {
         1, 0, 0, 0,
         0, 1, 0, y,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -69,7 +75,7 @@ const translateZ = (z) => {
         1, 0, 0, 0,
         0, 1, 0, 0,
         0, 0, 1, z,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -82,7 +88,7 @@ const skew = (α, β = α) => {
         1, tan(α), 0, 0,
         tan(β), 1, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -92,7 +98,7 @@ const skewX = (α) => {
         1, tan(α), 0, 0,
         0, 1, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -102,7 +108,7 @@ const skewY = (β) => {
         1, 0, 0, 0,
         tan(β), 1, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -112,7 +118,7 @@ const scale = (scalex, scaley = scalex) => {
         scalex, 0, 0, 0,
         0, scaley, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -121,7 +127,7 @@ const scale3d = (scalex = 1, scaley = 1, scalez = 1) => {
         scalex, 0, 0, 0,
         0, scaley, 0, 0,
         0, 0, scalez, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -130,7 +136,7 @@ const scaleX = (scalex) => {
         scalex, 0, 0, 0,
         0, 1, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -139,7 +145,7 @@ const scaleY = (scaley) => {
         1, 0, 0, 0,
         0, scaley, 0, 0,
         0, 0, 1, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -148,7 +154,7 @@ const scaleZ = (scalez) => {
         1, 0, 0, 0,
         0, 1, 0, 0,
         0, 0, scalez, 0,
-        0, 0, 0, 0
+        0, 0, 0, 1
     ]
 }
 
@@ -159,17 +165,14 @@ const scaleZ = (scalez) => {
  * @param y <number> 0 ~ 1
  * @param z <number> 0 ~ 1
  * @param deg <angle>
+ * 幂运算符（**）ECMAScript 2016 (ES 7) 新特性
  */
 const rotate3d = (x, y, z, deg) => {
-    const α = toRad(deg)
-    const sc = sin(α/2)*cos(α/2)
-    const sq = (sin(α/2))^2
-    return [
-        1 - 2 * sq * (y^2 + z^2), 2 * (x * y * sq  - z * sc), 2 * (x * z * sq  + y * sc), 0,
-        2 * (x * y * sq  + z * sc), 1 - 2 * (x^2 + z^2) * sq, 2 * (y * z * sq  - x * sc), 0,
-        2 * (x * z * sq  - y * sc), 2 * (y * z * sq + x * sc) * sq, 1 - 2 * sq * (x^2 + y^2),, 0,
-        0, 0, 0, 1
-    ]
+    const rad = toRad(deg)
+    const radZ = -atan(y / x)
+    const radY = -atan(sqrt(x * x + y * y) / z)
+
+    return combine3d(rotateZ(-radZ), rotateY(-radY), rotateZ(rad),  rotateY(radY), rotateZ(radZ))
 }
 
 const rotateX = (deg) => {
@@ -209,23 +212,24 @@ const rotateZ = (deg) => {
  * @param m 
  */
 const getCoordinateTransform = (x, y, m) => {
-    // m * [x,y,1]
+    // m * [x,y,1,1]
     return {
-        x: m[0]*x+m[1]*y+m[2],
-        y: m[3]*x+m[4]*y+m[5]
+        x: m[0]*x+m[1]*y+m[3],
+        y: m[4]*x+m[5]*y+m[7]
     }
 }
 
 /**
  * getStyleMatrix2d 矩阵转换 css matrix
- * @param m  [a, c, e, b, d, f, 0, 0, 1]  => [a, b, c, d, e, f]
+ * @param m  [a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4, d1, d2, d3, d4]  => (a1, b1, c1, d1, a2, b2, c2, d2, a3, b3, c3, d3, a4, b4, c4, d4)
  */
-const getStyleMatrix2d = (m = Matrix3d) => {
-    // m * [x,y,1]
+const getStyleMatrix3d = (m = Matrix3d) => {
     return [
-        m[0], m[3], m[1],
-        m[4], m[2], m[5]
-    ]
+        m[0], m[4], m[8], m[12],
+        m[1], m[5], m[9], m[13],
+        m[2], m[6], m[10], m[14],
+        m[3], m[7], m[11], m[15]
+    ].join(',')
 }
 
 
@@ -248,6 +252,6 @@ export {
     rotateY,
     rotateZ,
     getCoordinateTransform,
-    getStyleMatrix2d,
+    getStyleMatrix3d,
     combine3d,
 }
